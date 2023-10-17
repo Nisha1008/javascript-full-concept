9778d0f44d0e8e26cfbe8bd99c29722533b8a6ac
Nisha_Rani <104624740+Nisha1008@users.noreply.github.com>
2023-10-17 15:28:06 +0000

    datatypes

---
+++ 01basics1/01variavles.js
@@ -0,0 +1,15 @@
+const accountId = 4333
+let accountEmail = "nisha@gmail.com"
+var accountPassword ="1232"
+accountcity = "jasipaur"
+accountEmail="jsshd"
+accountPassword = "87"
+accountcity = "bengluru"
+let account; ///output-> undefined
+//  accountId=1;
+ console.log(accountId);
+ console.table([accountId,accountEmail,accountPassword,accountcity,account]); //print output in table format
+ /*
+ prefer not to uuse  var
+ beacuse of issue in block scope and funciton scope
+ */
---
+++ 01basics1/02variables.js
@@ -0,0 +1,20 @@
+ "use strict"; //treat all JS Code as newer version
+
+//  alert("hello") we are using node  js not browser
+console.log(3+3);
+
+console.log("nisha");
+
+let name ="nisha"
+let age = 15
+let isLoggedIn = true
+// null standlone value //null is a object
+//undefined => 
+// number
+// bigint
+// string
+// boolean
+// symbol
+// object
+console.log(typeof "hitesh");
+console.log(typeof undefined);
