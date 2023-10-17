1341888b8e64469dae46ba1ee9680c71c7f7bedb
Nisha_Rani <104624740+Nisha1008@users.noreply.github.com>
2023-10-17 15:42:53 +0000

    conversion

---
+++ 01basics1/03conversionoperation.js
@@ -0,0 +1,19 @@
+let score =true
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// "33" => 33
+// "33abc" =NaN
+// true =>1
+let isLoggedIn = true
+let booleanLoggedIn = Boolean(isLoggedIn)
+console.log(booleanLoggedIn);
+console.log(typeof booleanLoggedIn);
+
+let soneNumber = 33
+let stringNumber = String(soneNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
