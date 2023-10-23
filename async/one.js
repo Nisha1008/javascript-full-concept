41ebc9a61dae9843d704c8023c99b613b8774b75
Nisha_Rani <104624740+Nisha1008@users.noreply.github.com>
2023-10-23 16:30:32 +0000

    advance topics

---
+++ async/one.js
@@ -0,0 +1,14 @@
+// javascript is synchrounous
+// it is single threded
+
+// each operation wait for last one to complete before executing
+// execution context
+// execute one line of code at a time
+/*
+console.log ->1
+console.log ->2
+
+call stack ,heap memory
+*/
+
+// blobking vs nonblocking
